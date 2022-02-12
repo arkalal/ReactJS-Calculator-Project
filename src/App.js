29c8e0b74230c33075ea037f944eb21da4cf316c
arkalal <arkalal.123@gmail.com>
2022-02-12 23:52:56 +0530

    base UI developed and created dark and light mode logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import KeyPad from './components/KeyPad/KeyPad'
 
 const App = () => {
+
+    const [IsDark, setIsDark] = useState(false)
+
     return (
-        <div>
-            <h2>Lets Build Calculator 🔥🚀</h2>
+        <div className='app' data-theme={IsDark ? 'dark' : ''}>
+            <div className="appCalculator">
+                <div className="appCalculatorNavbar">
+                    <div className="appCalculatorNavbarToggle" onClick={() => { setIsDark(!IsDark) }}>
+                        <div className={IsDark ? 'appCalculatorNavbarToggleCircle appCalculatorActiveToggle' : 'appCalculatorNavbarToggleCircle'}></div>
+                    </div>
+
+                    <img src={IsDark ? '/assets/moon.png' : '/assets/sun.png'} alt="" />
+                </div>
+
+                <Header></Header>
+                <KeyPad></KeyPad>
+            </div>
         </div>
     )
 }
---
+++ src/components/KeyPad/KeyPad.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './KeyPad.css'
+
+const KeyPad = () => {
+    return (
+        <div className='keypad'>KeyPad</div>
+    )
+}
+
+export default KeyPad
