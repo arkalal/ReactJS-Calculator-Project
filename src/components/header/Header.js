f449341aa0b81d4ca92280575f4994cce89905b6
arkalal <arkalal.123@gmail.com>
2022-02-14 12:16:05 +0530

    header styled

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,23 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header'>Header</div>
+        <div className='header customScroll'>
+            <div className="headerHistory">
+                <p>10 + 2</p>
+                <p>10 + 3</p>
+                <p>10 + 5</p>
+            </div>
+
+            <br />
+
+            <div className="headerExpression customScroll">
+                <p>10 + 56</p>
+            </div>
+
+            <br />
+
+            <p className="headerResult">150</p>
+        </div>
     )
 }
 
